867fea0acc3bb327ae349a5553db487d4e4d38ce
danil <61111955+danilwhale@users.noreply.github.com>
2024-04-08 22:18:40 +0300

    initial commit

---
+++ main.c
@@ -0,0 +1,196 @@
+#define GRAPHICS_API_OPENGL_11
+
+#include "raylib.h"
+#include "raymath.h"
+
+#define BLOCKAGE_AMOUNT 10
+#define EARTHQUAKE_PERIOD 1.0
+
+typedef struct {
+    int x;
+    bool isInverted;
+} Blockage;
+
+Color invertColor(Color color);
+
+Color lerpColor(Color a, Color b, float time);
+
+void createNewBlockage(int index);
+
+void updatePlayer();
+
+void drawPlayer();
+
+void updateGame();
+
+void drawGame();
+
+Camera2D camera = {0};
+
+bool isInverted = false;
+Color accent = BLACK;
+Color target = BLACK;
+
+Blockage blockages[BLOCKAGE_AMOUNT];
+
+bool dead = false;
+int score = 0;
+
+double lastEarthquakeTime = 0.0;
+
+int main() {
+    InitWindow(800, 600, "invert");
+    SetTargetFPS(60);
+
+    camera.rotation = 7.5f;
+    camera.offset = (Vector2) {
+            (float) GetScreenWidth() / 2,
+            (float) GetScreenHeight() / 2
+    };
+    camera.target = (Vector2) {
+            (float) GetScreenWidth() / 2,
+            (float) GetScreenHeight() / 2
+    };
+    camera.zoom = 1.0f;
+
+    for (int i = 0; i < BLOCKAGE_AMOUNT; i++) {
+        createNewBlockage(i);
+    }
+
+    while (!WindowShouldClose()) {
+        if (!dead) updateGame();
+
+        BeginDrawing();
+
+        drawGame();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
+
+Color invertColor(Color color) {
+    return (Color) {255 - color.r, 255 - color.g, 255 - color.b, 255};
+}
+
+Color lerpColor(Color a, Color b, float time) {
+    return ColorFromNormalized(
+            (Vector4) {
+                    Lerp((float) a.r / 255.0f, (float) b.r / 255.0f, time),
+                    Lerp((float) a.g / 255.0f, (float) b.g / 255.0f, time),
+                    Lerp((float) a.b / 255.0f, (float) b.b / 255.0f, time),
+                    1.0f
+            });
+}
+
+void createNewBlockage(int index) {
+    Blockage *blockage = &blockages[index];
+    blockage->x = GetScreenWidth() + index * 150;
+    blockage->isInverted = GetRandomValue(0, 1) > 0;
+
+    for (int i = 0; i < BLOCKAGE_AMOUNT; i++) {
+        Blockage other = blockages[i];
+        if (fabsf((float) other.x - (float) blockage->x) < 25.0f &&
+            ((blockage->isInverted && !other.isInverted) ||
+             (!blockage->isInverted && other.isInverted))) {
+            blockage->x += 150;
+        }
+    }
+}
+
+void updatePlayer() {
+    int quarter = GetScreenWidth() / 4;
+    int y = isInverted ? GetScreenHeight() / 2 : GetScreenHeight() / 2 - 64;
+    Rectangle rec = (Rectangle) {(float) quarter, (float) y, 16.0f, 64.0f};
+
+    for (int i = 0; i < BLOCKAGE_AMOUNT; i++) {
+        Blockage blockage = blockages[i];
+        Rectangle blockageRec = (Rectangle) {
+                (float) blockage.x,
+                (float) (blockage.isInverted ? GetScreenHeight() / 2 : GetScreenHeight() / 2 - 128),
+                1.0f,
+                128.0f
+        };
+
+        if (CheckCollisionRecs(rec, blockageRec)) {
+            dead = true;
+        }
+    }
+}
+
+void drawPlayer() {
+    int quarter = GetScreenWidth() / 4;
+
+    DrawRectangleLines(
+            quarter,
+            isInverted ? GetScreenHeight() / 2 : GetScreenHeight() / 2 - 64,
+            16,
+            64,
+            accent);
+}
+
+void updateGame() {
+    camera.rotation = Lerp(camera.rotation, 7.5f, 0.25f);
+    accent = lerpColor(accent, target, 0.2f);
+
+    if (GetTime() - lastEarthquakeTime > EARTHQUAKE_PERIOD) {
+        camera.rotation += 2.0f * (GetRandomValue(0, 1) > 0 ? -1.0f : 1.0f);
+        lastEarthquakeTime = GetTime();
+    }
+
+    if (IsKeyPressed(KEY_SPACE)) {
+        isInverted = !isInverted;
+        target = isInverted ? WHITE : BLACK;
+
+        camera.rotation += (float) GetRandomValue(2, 8);
+    }
+
+    for (int i = 0; i < BLOCKAGE_AMOUNT; i++) {
+        Blockage *blockage = &blockages[i];
+        blockage->x -= 5;
+
+        if (blockage->x <= 0) {
+            createNewBlockage(i);
+            score++;
+        }
+    }
+
+    updatePlayer();
+}
+
+void drawGame() {
+    ClearBackground(invertColor(accent));
+
+    BeginMode2D(camera);
+
+    DrawLine(
+            -GetScreenWidth() / 2,
+            GetScreenHeight() / 2,
+            GetScreenWidth() * 2,
+            GetScreenHeight() / 2,
+            accent);
+
+    for (int i = 0; i < BLOCKAGE_AMOUNT; i++) {
+        Blockage blockage = blockages[i];
+        DrawRectangle(
+                blockage.x,
+                blockage.isInverted ? GetScreenHeight() / 2 : GetScreenHeight() / 2 - 128,
+                1,
+                128,
+                accent
+        );
+    }
+
+    drawPlayer();
+
+    DrawText(TextFormat("score: %d", score), 64, 64, 32, accent);
+
+    if (dead) {
+        int width = MeasureText("you died", 48);
+        DrawText("you died", GetScreenWidth() / 2 - width / 2, GetScreenHeight() / 2 - 48 / 2, 48, accent);
+    }
+
+    EndMode2D();
+}
